4ec272029180fb3aa2ba6f89c995b4e158db2849
rodasalfaro <58200039+rodasalfaro@users.noreply.github.com>
2021-10-03 22:42:13 -0600

    Add files via upload

---
+++ passtest.py
@@ -0,0 +1,12 @@
+
+name = input('Ingresa tu nombre :')
+pass1 = input('Ingresa tu password :')
+
+longpass = len(pass1)
+passhidden = '*' * longpass
+
+print(f'password {passhidden} is {longpass} letters long')
+
+
+#print('*' *10)
+
